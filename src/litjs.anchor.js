37ac32cf07c7801586802a2efecfa94f34d3a729
lychie <lychie@yeah.net>
2014-08-09 21:04:06 +0800

    js

---
+++ src/litjs.anchor.js
@@ -0,0 +1,29 @@
+var Litjs = (function($){
+	$.Anchor = {
+		set : function(){
+			var len = arguments.length;
+			if(len == 0)
+				return ;
+			var millisecond = 1000;
+			if(len > 1 && !isNaN(arguments[len - 1])){
+				len -= 1;
+				millisecond = arguments[len];
+			}
+			for(var i = 0; i < len; i++){
+				jQuery(arguments[i]).anchorset(millisecond);
+			}
+		}
+	}
+	jQuery.fn.anchorset = function(millisecond){
+		var _this = jQuery(this), _position, _link;
+		_this.each(function(i){
+			jQuery(_this[i]).click(function(){
+				_link = jQuery(this).attr("href").substr(1);
+				_position = jQuery("#" + _link).offset().top;
+				jQuery("html,body").animate({scrollTop : _position}, millisecond);
+				return false;
+			});
+		});
+	};
+	return $;
+})(Litjs || {});
---
+++ src/litjs.cookie.js
@@ -0,0 +1,33 @@
+var Litjs = (function($){
+	$.Cookie = {
+		add : function(name, value, expire_minutes){
+			var expires = "";
+			if(expire_minutes != undefined && expire_minutes != 0){
+				var curr_date = new Date();
+				curr_date.setTime(curr_date.getTime() + expire_minutes * 60 * 1000);
+				expires = ";expires=" + curr_date.toGMTString();
+			}
+			document.cookie = name + "=" + escape(value) + expires;
+		},
+		get : function(name){
+			var value;
+			if(document.cookie.length > 0){
+				var index = document.cookie.indexOf(name + "=");
+				if(index != -1){
+					var startIndex = index + name.length + 1;
+					var endIndex = document.cookie.indexOf(";", startIndex);
+					if(endIndex == -1)
+						endIndex = document.cookie.length;
+					value = unescape(document.cookie.substring(startIndex, endIndex));
+				}
+			}
+			return value;
+		},
+		del : function(name){
+			var value = this.get(name);
+			if(value != undefined)
+				this.add(name, value, -1);
+		}
+	};
+	return $;
+})(Litjs || {});
